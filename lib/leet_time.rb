fa281e06002b87ef51fe85b6df6bc4a99a50013f
nicho1991 <ladefoged1991@gmail.com>
2024-03-24 21:05:17 +0100

    support for timezone

--- lib/leet_time.rb
+++ lib/leet_time.rb
@@ -4,28 +4,37 @@ require 'active_support/all'
 
 # You can pass in a time, the time will be checked if it was or was not leet in the timezone given!
 class LeetTime
-  def initialize; end
+  attr_reader :timezone
+
+  def initialize(timezone: Time.zone)
+    @timezone = timezone
+  end
 
   def self.greet
     '1337 71m3'
   end
 
-  def leet_time?(time)
-    @leet_time_with_zone = time.in_time_zone
-    @leet_time_with_zone.hour == 13 && @leet_time_with_zone.min == 37
+  def leet_time?(time = Time.current)
+    time.hour == 13 && time.min == 37
   end
 
   def now_before_leet?
-    lt = Time.now.in_time_zone.change(hour: 13, min: 37)
-    ct = Time.now.in_time_zone
+    lt = Time.current.in_time_zone(timezone).change(hour: 13, min: 37)
+    ct = Time.current
 
-    ct.before?(lt)
+    ct_seconds = ct.hour.hours + ct.min.minutes
+    lt_seconds = lt.hour.hours + lt.min.minutes
+
+    ct_seconds < lt_seconds
   end
 
   def now_after_leet?
-    lt = Time.now.in_time_zone.change(hour: 13, min: 37)
-    ct = Time.now.in_time_zone
+    lt = Time.current.in_time_zone(timezone).change(hour: 13, min: 37)
+    ct = Time.current
 
-    ct.after?(lt)
+    ct_seconds = ct.hour.hours + ct.min.minutes
+    lt_seconds = lt.hour.hours + lt.min.minutes
+
+    ct_seconds > lt_seconds
   end
 end
--- test/test_leet_time.rb
+++ test/test_leet_time.rb
@@ -4,6 +4,7 @@ require 'minitest/autorun'
 require 'leet_time'
 require 'active_support/testing/time_helpers'
 include ActiveSupport::Testing::TimeHelpers
+
 class LeetTimeTest < Minitest::Test
   def test_greet
     assert_equal '1337 71m3',
@@ -16,17 +17,18 @@ class LeetTimeTest < Minitest::Test
   end
 
   def test_before_leet?
-    travel_to Time.new(1337, 1, 1, 01, 01, 00)
+    Time.zone = 'UTC'
+    travel_to Time.zone.local(1337, 1, 1, 01, 01, 00)
     assert_same(LeetTime.new.now_before_leet?, true)
     assert_same(LeetTime.new.now_before_leet?, true)
     assert_same(LeetTime.new.now_before_leet?, true)
     assert_same(LeetTime.new.now_before_leet?, true)
 
 
-    travel_to Time.new(1337, 1, 1, 13, 37, 00)
+    travel_to Time.zone.local(1337, 1, 1, 13, 37, 00)
     assert_same(LeetTime.new.now_before_leet?, false)
 
-    travel_to Time.new(1337, 1, 1, 13, 38, 00)
+    travel_to Time.zone.local(1337, 1, 1, 13, 38, 00)
     assert_same(LeetTime.new.now_before_leet?, false)
   end
 
@@ -42,5 +44,33 @@ class LeetTimeTest < Minitest::Test
 
     travel_to Time.new(1337, 1, 1, 13, 38, 00)
     assert_same(LeetTime.new.now_after_leet?, true)
+  end
+
+  def test_provide_timezone
+    tz = "Eastern Time (US & Canada)"
+    leet_time = LeetTime.new(timezone: tz)
+    assert_same(leet_time.timezone, tz)
+  end
+
+  def test_leet_in_different_timezone
+    Time.zone = 'UTC'
+    # test that leet works
+    travel_to Time.zone.local(2024, 1, 1, 13, 37, 00)
+    leet_time = LeetTime.new(timezone: 'UTC')
+    assert_equal(true, leet_time.leet_time?(Time.current))
+
+    # +1
+    # UTC 13:37, meaning CET 14:37 (+1)
+    Time.zone = 'CET'
+    assert_same(false, leet_time.leet_time?)
+    assert_same(true, leet_time.now_after_leet?)
+    assert_same(false, leet_time.now_before_leet?)
+
+    # -1
+    # UTC 13:37, meaning Cape_Verde 12:37 (-1)
+    Time.zone = 'Atlantic/Cape_Verde'
+    assert_same(false, leet_time.leet_time?(Time.current))
+    assert_same(false, leet_time.now_after_leet?)
+    assert_same(true, leet_time.now_before_leet?)
   end
 end
